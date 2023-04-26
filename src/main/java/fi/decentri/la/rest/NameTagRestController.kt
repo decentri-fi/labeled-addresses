785224402d6bc8d510e2fde35d8dabf06ac573d4
Qkyrie <quintendeswaef@gmail.com>
2023-04-26 13:52:01 +0200

    nametags

--- src/main/java/fi/decentri/la/rest/NameTagRestController.kt
+++ src/main/java/fi/decentri/la/rest/NameTagRestController.kt
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/nametags")
+@RequestMapping("/")
 class NameTagRestController(
     private val nameTagService: NameTagService
 ) {
