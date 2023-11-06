00045f1180fcaecb188b61237672e4d4af3ec1ef
Quinten De Swaef <quintendeswaef@gmail.com>
2023-11-06 12:09:32 +0100

    update networks

--- src/main/java/fi/decentri/la/domain/Network.kt
+++ src/main/java/fi/decentri/la/domain/Network.kt
@@ -5,6 +5,6 @@ enum class Network(val slug: String, val logo: String, val baseUrl: String, val 
     OPTIMISM("optimism", "optimism.png", "https://optimistic.etherscan.io", 10),
     ARBITRUM("arbitrum", "arbitrum.png", "https://arbiscan.io", 42161),
     POLYGON("polygon", "polygon.png", "https://polygonscan.com", 137),
-    POLYGON_ZKEVM("polygon-zkevm", "polygon-zkevm.png", 1101),
-    BASE("base", "base.png", 8453);
+    POLYGON_ZKEVM("polygon-zkevm", "polygon-zkevm.png", "https://zkevm.polygonscan.com", 1101),
+    BASE("base", "base.png", "https://basescan.org/", 8453);
 }
