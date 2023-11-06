9a0f883c98648ac0186bde60029a693321072b71
Quinten De Swaef <quintendeswaef@gmail.com>
2023-11-06 12:04:26 +0100

    update networks

--- src/main/java/fi/decentri/la/domain/Network.kt
+++ src/main/java/fi/decentri/la/domain/Network.kt
@@ -4,9 +4,7 @@ enum class Network(val slug: String, val logo: String, val baseUrl: String, val 
     ETHEREUM("ethereum", "ethereum.png", "https://etherscan.io", 1),
     OPTIMISM("optimism", "optimism.png", "https://optimistic.etherscan.io", 10),
     ARBITRUM("arbitrum", "arbitrum.png", "https://arbiscan.io", 42161),
-    FANTOM("fantom", "fantom.png", "https://ftmscan.com", 250),
-    AVALANCHE("avalanche", "avalanche.png", "https://cchain.explorer.avax.network", 43114),
-    BINANCE("binance", "bsc.svg", "https://bscscan.com", 56),
     POLYGON("polygon", "polygon.png", "https://polygonscan.com", 137),
-    STARKET("starknet.png", "starknet", "https://starknet.io", 0),
+    POLYGON_ZKEVM("polygon-zkevm", "polygon-zkevm.png", 1101),
+    BASE("base", "base.png", 8453);
 }
